bbdbb405906c98665dae7f62df881024b6670f60
Endy Muhardin <endy.muhardin@gmail.com>
2024-04-30 11:01:42 +0700

    add anotasi entity dan getter/setter

--- toko-ejb/src/main/java/com/muhardin/endy/training/jee6/entity/Pelanggan.java
+++ toko-ejb/src/main/java/com/muhardin/endy/training/jee6/entity/Pelanggan.java
@@ -1,5 +1,17 @@
 package com.muhardin.endy.training.jee6.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity @Data
 public class Pelanggan {
 
+    @Id
+    private String id;
+    private String nama;
+    private String email;
+    
+    
 }
