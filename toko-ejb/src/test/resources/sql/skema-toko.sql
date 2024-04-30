19c746724c8deb224d3272485f5010211fca8961
Endy Muhardin <endy.muhardin@gmail.com>
2024-04-30 10:49:42 +0700

    starter skema database

---
+++ toko-ejb/src/test/resources/sql/skema-toko.sql
@@ -0,0 +1,19 @@
+create table produk (
+    id varchar(36),
+    primary key (id)
+);
+
+create table pelanggan (
+    id varchar(36),
+    primary key (id)
+);
+
+create table pembelian (
+    id varchar(36),
+    primary key (id)
+);
+
+create table pembayaran (
+    id varchar(36),
+    primary key (id)
+);
