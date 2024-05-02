89f4e5f9479b95f12a67ab6947ca63aaa8fd8dcc
Endy Muhardin <endy.muhardin@gmail.com>
2024-05-02 14:41:19 +0700

    class Application supaya REST endpoint autodetect pada saat deploy

---
+++ aplikasi-toko/toko-rest/src/main/java/com/muhardin/endy/training/jee6/controller/rest/TokoRestApplication.java
@@ -0,0 +1,9 @@
+package com.muhardin.endy.training.jee6.controller.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class TokoRestApplication extends Application {
+    
+}
