b8aaf15b11f6fd1c26b74c8b3e9efe30f0fdf8f0
Endy Muhardin <endy.muhardin@gmail.com>
2024-05-02 14:56:53 +0700

    add query param

--- aplikasi-toko/toko-rest/src/main/java/com/muhardin/endy/training/jee6/controller/rest/PelangganApi.java
+++ aplikasi-toko/toko-rest/src/main/java/com/muhardin/endy/training/jee6/controller/rest/PelangganApi.java
@@ -3,9 +3,11 @@ package com.muhardin.endy.training.jee6.controller.rest;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import com.muhardin.endy.training.jee6.dao.PelangganDao;
 import com.muhardin.endy.training.jee6.entity.Pelanggan;
@@ -18,9 +20,8 @@ public class PelangganApi {
     @GET
     @Path("/")
     @Produces("application/json")
-    public List<Pelanggan> semuaPelanggan(){
-        Integer start = 0;
-        Integer rows = 10;
+    public List<Pelanggan> semuaPelanggan(@DefaultValue("0") @QueryParam("start") Integer start, 
+                                            @DefaultValue("10")@QueryParam("rows") Integer rows){
         return pelangganDao.cariPelanggan(start, rows);
     }
 }
