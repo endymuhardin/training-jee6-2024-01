f6e12dd15f09c3fdf6a30677312d669a930c279d
Endy Muhardin <endy.muhardin@gmail.com>
2024-05-06 14:02:46 +0700

    starter spring boot

---
+++ toko-spring/src/main/java/com/muhardin/endy/training/jee6/tokospring/TokoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.training.jee6.tokospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoSpringApplication.class, args);
+	}
+
+}
---
+++ toko-spring/src/test/java/com/muhardin/endy/training/jee6/tokospring/TokoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.training.jee6.tokospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
