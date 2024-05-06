2d29464319d40c2ce94e2c43434e3cb3a85321d1
Endy Muhardin <endy.muhardin@gmail.com>
2024-05-06 11:58:56 +0700

    contoh hasil generate wsdl

--- aplikasi-toko/toko-soap/src/main/java/com/muhardin/endy/training/jee6/controller/soap/PembayaranApi.java
+++ aplikasi-toko/toko-soap/src/main/java/com/muhardin/endy/training/jee6/controller/soap/PembayaranApi.java
@@ -8,10 +8,16 @@ import javax.jws.WebService;
 
 import com.muhardin.endy.training.jee6.dao.PembayaranDao;
 import com.muhardin.endy.training.jee6.dto.RekapPembayaran;
+import com.muhardin.endy.training.jee6.entity.PembayaranVirtualAccount;
 
 @WebService
 public class PembayaranApi {
     @EJB private PembayaranDao pembayaranDao;
+
+    @WebMethod
+    public void simpanPembayaranVirtualAccount(PembayaranVirtualAccount va){
+        pembayaranDao.simpan(va);
+    }
 
     @WebMethod
     public List<RekapPembayaran> tampilkanRekapPembayaran(){
